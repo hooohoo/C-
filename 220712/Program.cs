f0767fa1c934a31624a703a5ac74edc5e96783d8
hphptop <hphptop@naver.com>
2022-07-12 15:15:47 +0900

    220712 : First Test

---
+++ 220712/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _220712
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.Write("여러분 ");
+            Console.WriteLine("안녕하세요");
+            Console.WriteLine("안녕하세요");
+            Console.WriteLine("안녕하세요");
+            */
+            int score = 1000000000;
+            Console.WriteLine(score);
+            Console.WriteLine("스코어 : " + score);
+            Console.WriteLine("스코어 : {0}" + score);
+        }
+    }
+}
